603727b7403d69fc020f3ccf6b8ae7931a582d4e
Jai Mungi <jaimungi@gmail.com>
2026-07-10 15:28:33 +0530

    dbutil changed

--- src/main/java/com/util/DBUtil.java
+++ src/main/java/com/util/DBUtil.java
@@ -16,23 +16,24 @@ public class DBUtil {
 	static Connection con;
 	public static Connection getConnection()
 	{
-		String host = System.getenv("MYSQLHOST");
-		String port = System.getenv("MYSQLPORT");
-		String database = System.getenv("MYSQLDATABASE");
-		String user = System.getenv("MYSQLUSER");
-		String password = System.getenv("MYSQLPASSWORD");
-//		String url = "jdbc:mysql://" + host + ":" + port + "/" + database
-//		           + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-		String url = "jdbc:mysql://mysql.railway.internal:3306/railway";
-		try
-		{
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(url,user,password);
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		return con;
+		try {
+
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+
+	        String host = System.getenv("MYSQLHOST");
+	        String port = System.getenv("MYSQLPORT");
+	        String database = System.getenv("MYSQLDATABASE");
+	        String user = System.getenv("MYSQLUSER");
+	        String password = System.getenv("MYSQLPASSWORD");
+
+	        String url = "jdbc:mysql://" + host + ":" + port + "/" + database
+	                + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+	        return DriverManager.getConnection(url, user, password);
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return null;
+	    }
 	}
 }
