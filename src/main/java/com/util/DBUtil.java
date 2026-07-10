e9e24b00762605f484d8558f6fdf810c37cd004e
Jai Mungi <jaimungi@gmail.com>
2026-07-10 15:15:01 +0530

    DBUtil

--- src/main/java/com/util/DBUtil.java
+++ src/main/java/com/util/DBUtil.java
@@ -9,19 +9,29 @@ public class DBUtil {
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 //    private static final String URL =
 //            "jdbc:mysql://localhost:3306/studentdb";
-	private static final String URL = "mysql://root:ZUIRNkkqxvOpsrENDQHsMOLaMpgiOFfz@mysql.railway.internal:3306/railway";
-	private static final String USER = "root";
-	private static final String PASSWORD = "ZUIRNkkqxvOpsrENDQHsMOLaMpgiOFfz"; // <-- change to your MySQL password
-
-	static {
-		try {
-			Class.forName(DRIVER);
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("MySQL JDBC Driver not found. Add mysql-connector-j jar to WEB-INF/lib.", e);
+	
+//	private static final String USER = "root";
+//	private static final String PASSWORD = "ZUIRNkkqxvOpsrENDQHsMOLaMpgiOFfz"; // <-- change to your MySQL password
+	
+	static Connection con;
+	public static Connection getConnection()
+	{
+		String host = System.getenv("MYSQLHOST");
+		String port = System.getenv("MYSQLPORT");
+		String database = System.getenv("MYSQLDATABASE");
+		String user = System.getenv("MYSQLUSER");
+		String password = System.getenv("MYSQLPASSWORD");
+		String url = "jdbc:mysql://" + host + ":" + port + "/" + database
+		           + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(url,user,password);
 		}
-	}
-
-	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(URL, USER, PASSWORD);
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return con;
 	}
 }
